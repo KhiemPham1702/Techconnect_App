788e182af51c723ecf4b4a705a085fe77fbad311
Thien260102 <nguyenvanthien18209@gmail.com>
2023-04-10 17:51:08 +0700

    Fix some bugs relate SVG and Image

--- components/DAL/ProductDAL.js
+++
@@ -1,7 +0,0 @@
-
-import { db, collection, addDoc, getDoc, getDocs, query, doc } from '../DAL/Database'
-
-const q = query(collection(db, "Techconnect"));
-const myProducts = getDocs(q);
-
-export { myProducts, doc, getDocs};
